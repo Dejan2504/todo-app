4af45089264ccdd92946e7b564d507418cbc9871
Dejan2504 <krajisnikdejan@gmail.com>
2022-12-23 00:36:27 +0100

    local storage saving

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import './App.css';
 import Header from './components/Header';
 
 function App() {
-  const [list, setList] = useState([]);
+  //Setting initial state
+  const localTodos = localStorage.getItem('todoList') ? JSON.parse(localStorage.getItem('todoList')) : []
+
+  const [list, setList] = useState(localTodos);
   const [input, setInput] = useState('');
 
+  //Setting new data in localStorage
+  useEffect(() => {
+    localStorage.setItem('todoList', JSON.stringify(list));
+  },[list]);
+  
   const addTodo = function(todo){
     //creating objects for list items
     const newTodo = {
       id: Math.random(),
       todo: todo
     }
-
+    
     //newList
     setList([...list, newTodo]);
-
+    
+    
     //clear input
     setInput('');
+    
   }
 
   const removeTodo = function(todoId){
@@ -31,16 +41,16 @@ function App() {
   return (
     <div className="App">
       <Header />
-     <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
-     <button onClick={() => addTodo(input)}>Add Todo</button>
-     <hr />
-    <ul>
-      {list.map((todo) => (
-        <li key={todo.id} onClick={() => removeTodo(todo.id)}>
-        {todo.todo}
-        </li> 
-      ))} 
-    </ul>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={() => addTodo(input)}>Add Todo</button>
+      <hr />
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id} onClick={() => removeTodo(todo.id)}>
+            {todo.todo}
+          </li> 
+        ))} 
+      </ul>
     </div>
   );
 }
