6c4077cc2d02638763dadb1af6ce73dd30e57f94
Dejan2504 <krajisnikdejan@gmail.com>
2022-12-22 21:06:13 +0100

    Finised app - next adding Context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = function(todo){
+    //creating objects for list items
+    const newTodo = {
+      id: Math.random(),
+      todo: todo
+    }
+
+    //newList
+    setList([...list, newTodo]);
+
+    //clear input
+    setInput('');
+  }
+
+  const removeTodo = function(todoId){
+    //filter arr to remove exact todo
+    const newList = list.filter(todo => todo.id !== todoId);
+
+    //set new list
+    setList(newList);
+  }
+
   return (
     <div className="App">
-     Todo app
+      <Header />
+     <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+     <button onClick={() => addTodo(input)}>Add Todo</button>
+     <hr />
+    <ul>
+      {list.map((todo) => (
+        <li key={todo.id} onClick={() => removeTodo(todo.id)}>
+        {todo.todo}
+        </li> 
+      ))} 
+    </ul>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import classes from './Header.module.css';
+
+const Header = function(){
+    return(
+        <h1 className={classes.title}>Todo app</h1>
+    )
+}
+
+export default Header;
